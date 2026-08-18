845220e682d0c87cc3bd7066977a0653613ad72f
ajsiragusa <ajsirag@gmail.com>
2026-08-18 15:42:07 -0500

    ui update and base implementation for registration

--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  
+  private apiURL = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient){}
+
+  register(username: string, password: string){
+    const body = {
+      "username": username,
+      "password": password
+    }
+
+    return this.httpClient.post<User>(this.apiURL + '/register', body);
+  }
+
 }
