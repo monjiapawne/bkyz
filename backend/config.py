93d8289c96a5730181be31c18c1de2240cb4e20e
ajsiragusa <ajsirag@gmail.com>
2026-08-18 16:43:12 -0500

    fixed cors again

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ class Config:
     SESSION_COOKIE_SAMESITE = "Lax"
     SESSION_COOKIE_SECURE = not DEBUG
     CORS_ALLOW_LIST = [
-        "http://localhost:4200"
+        "http://localhost:4200",
         "http://127.0.0.1:4200"
     ]
 
