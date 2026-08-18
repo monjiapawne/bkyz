125d2d283aa27eec4175a983d0dc1113bdcb3e11
ajsiragusa <ajsirag@gmail.com>
2026-08-18 01:38:26 -0500

    changed over to angular and created login and register pages

---
+++ frontend/src/app/components/login-page/login-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './login-page.html',
+  styleUrl: './login-page.css',
+})
+export class LoginPage {
+
+}
