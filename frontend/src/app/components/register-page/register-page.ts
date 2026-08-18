5df8f4626a8a607ee8a4e005a9387e1e84f0f620
ajsiragusa <ajsirag@gmail.com>
2026-08-18 15:42:07 -0500

    ui update and base implementation for registration

--- frontend/src/app/components/register-page/register-page.ts
+++ frontend/src/app/components/register-page/register-page.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../services/auth';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
 export class RegisterPage {
 
+  constructor(private auth: Auth){}
+
+  username: string = '';
+  password: string = '';
+  userId: number = -1;
+
+  attemptRegistration() {
+    this.auth.register(this.username, this.password)
+    .subscribe(
+    {
+      next: responseData => { 
+        this.userId = responseData.id;
+        console.log(this.userId);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
 }
