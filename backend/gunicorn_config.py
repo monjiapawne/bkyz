28fe8cb7fa1f41ce724eb10079b6f34813ab71da
monjiapawne <187591672+monjiapawne@users.noreply.github.com>
2026-09-10 16:57:48 -0400

    clean up gunicorn config

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,7 +1,16 @@
 from os import environ as env
 
-bind = f":{int(env.get('PORT', '8000'))}"
+# Config for gunicorn. This file is somewhat a helper to make environment variables
+# have sensible defaults and move the weight to here rather than in the .env
+# ref: https://gunicorn.org/reference/settings/#access_log_format
+
+
+# Performance
 workers = int(env.get("WORKERS", "4"))
 
+# Config
+bind = f":{int(env.get('PORT', '8000'))}"
+
+# Logging
 accesslog = "-" if env.get("ACCESS_LOG") == "true" else None
 loglevel = env.get("LOG_LEVEL", "info")
